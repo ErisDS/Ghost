2440ca07be64a4161dba4b8b8922c4df6db026dc
Hannah Wolfe <github.erisds@gmail.com>
2021-07-07 20:51:41 +0100

    Moved vhost mounts into boot file
    - This stops the mounting of the admin and frontend from being buried deep in express initialisation
    - Instead it's explicit, which makes two things almost possible:
       1. we can potentially boot the frontend or backend independently
       2. we can pass services and settings loaded during boot into the frontend
    - This needs more work, but we can start to group all the frontend code together
    - Meanwhile we also need to rip apart the routing and url services to decouple the frontend from the backend fully
    - BABY STEPS!

--- core/server/web/parent/backend.js
+++ core/server/web/parent/backend.js
@@ -1,5 +1,6 @@
 const debug = require('@tryghost/debug')('web:backend');
 const express = require('../../../shared/express');
+const vhostUtils = require('./vhost-utils');
 
 /**
  *
@@ -10,9 +11,12 @@ module.exports = () => {
     // BACKEND
     // Wrap the admin and API apps into a single express app for use with vhost
     const backendApp = express('backend');
+    backendApp.set('vhostArg', vhostUtils.getBackendHostArg());
+
     backendApp.lazyUse('/ghost/api', require('../api'));
     backendApp.lazyUse('/ghost/oauth', require('../oauth'));
     backendApp.lazyUse('/ghost/.well-known', require('../well-known'));
+
     backendApp.use('/ghost', require('../../services/auth/session').createSessionFromToken, require('../admin')());
 
     return backendApp;
