ec4f9badeed8ee9773061a5a94b67b484b992e82
Hannah Wolfe <github.erisds@gmail.com>
2022-02-18 13:31:11 +0000

    Added pages

--- core/frontend/router/index.js
+++ core/frontend/router/index.js
@@ -1,14 +1,21 @@
 const router = require('../../shared/express').Router();
 const controllers = require('../controllers');
 
-router.get('/archive/', (req, res, next) => {
+const routeNames = {
+    archive: '/archive/',
+    authors: '/author/:slug',
+    pages: '/:slug/',
+    posts: '/:slug/'
+};
+
+router.get(routeNames.archive, (req, res, next) => {
     res.routerOptions = {
         type: 'channel'
     };
     return controllers.channel(req, res, next);
 });
 
-router.get('/author/:slug/', (req, res, next) => {
+router.get(routeNames.authors, (req, res, next) => {
     res.routerOptions = {
         filter: 'authors:\'%s\'',
         editRedirect: '#/settings/staff/:slug/',
@@ -17,7 +24,26 @@ router.get('/author/:slug/', (req, res, next) => {
     return controllers.channel(req, res, next);
 });
 
-router.get('/:slug/', (req, res, next) => {
+// Do pages
+router.get(routeNames.pages, (req, res, next) => {
+    res.routerOptions = {
+        type: 'entry',
+        permalinks: '/:slug/',
+        query: {
+            controller: 'pagesPublic',
+            type: 'read',
+            resource: 'pages',
+            options: {
+                slug: '%s'
+            }
+        }
+    };
+
+    return controllers.entry(req, res, next);
+});
+
+// Do posts
+router.get(routeNames.posts, (req, res, next) => {
     res.routerOptions = {
         type: 'entry',
         permalinks: '/:slug/',
